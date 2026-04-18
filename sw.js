0d2d4ba265ed17bd95b1f087007c784ca9fa5f3e
emmanuel002332 <emmanuelnnam002@gmail.com>
2026-04-18 15:56:01 +0100

    Add service worker for asset caching
    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'course-outline-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './practice.css',
+  './manifest.json',
+  './1st.html',
+  './2nd.html',
+  './3rd.html',
+  './4th.html',
+  './5th.html',
+  './6th.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
