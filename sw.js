235e01f792e0bc923ca616a2c525e1da1f0dd51c
emmanuel002332 <emmanuelnnam002@gmail.com>
2026-04-20 21:54:49 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'course-outline-v1';
+hereconst CACHE_NAME = 'course-outline-v1';
 const ASSETS = [
   './',
   './index.html',
@@ -12,26 +12,12 @@ const ASSETS = [
   './6th.html'
 ];
 
-// Install and cache files
 self.addEventListener('install', (event) => {
-  self.skipWaiting(); // Makes the new service worker take over immediately
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// CLEANUP: This deletes old caches so your app stays updated
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }));
-    })
-  );
-});
-
-// Serve files from cache
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((res) => res || fetch(event.request))
